b173fd9c2949da2bd01e656f494e09938b8e1fb7
MD. MAMUN HOSSAIN <49454465+mamunsust12@users.noreply.github.com>
2023-01-29 06:19:43 -0800

    Create 2_1_variables.c

---
+++ 02 - Variables and Data Types/2_1_variables.c
@@ -0,0 +1,16 @@
+/*
+ Filename : 02 - Variables and Data Types/2_1_variables.c
+Description : Variables and Data Type
+*/
+#include<stdio.h>
+
+int main()
+{
+    int a,b;
+    float c;
+    double d;
+    c=a/b;
+    d=a*b;
+   return 0;
+}
+
