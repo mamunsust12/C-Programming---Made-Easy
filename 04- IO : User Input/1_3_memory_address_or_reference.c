b31fd7a5fd6d4976d6ab7024088710c5011d6f20
MD. MAMUN HOSSAIN <49454465+mamunsust12@users.noreply.github.com>
2023-01-31 00:06:01 +0600

    Add files via upload

---
+++ 04- IO : User Input/1_3_memory_address_or_reference.c
@@ -0,0 +1,19 @@
+/*
+C Programming Made Easy
+---------------------------------------------------------------------
+Topic       : User Input
+Description : Memory Address/References
+Programmer  : Mohammed Mamun Hossain,
+              Asst. Prof., Dept of CSE, BAUST.
+			  mamunsust12@gmailcom/mhossain@baustedubd
+Date        : 30/01/2023
+---------------------------------------------------------------------
+*/
+
+#include <stdio.h>
+
+int main() {
+  int myAge = 43;
+  printf("%p", &myAge);
+  return 0;
+}
