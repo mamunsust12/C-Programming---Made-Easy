587eea4fb630c448a2add3c852521b9c68a9d041
MD. MAMUN HOSSAIN <49454465+mamunsust12@users.noreply.github.com>
2024-11-03 16:53:17 +0600

    Create EfficientStringReverseWithoutFunction.c

---
+++ 07 - String/EfficientStringReverseWithoutFunction.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    char str[] = "madam";  // The input string
+    int length = 5;        // Length of the string "madam"
+    int i;
+
+    // Print the original string
+    printf("Original string: %s\n", str);
+
+    // Reverse the string in-place
+    for (i = 0; i < length / 2; i++) {
+        // Swap characters from the beginning and the end
+        char temp = str[i];
+        str[i] = str[length - i - 1];
+        str[length - i - 1] = temp;
+    }
+
+    // Print the reversed string
+    printf("Reversed string: %s\n", str);
+
+    return 0;
+}
