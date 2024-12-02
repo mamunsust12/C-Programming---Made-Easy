8759802ff0b39168bac6ac9388a601eba0843340
MD. MAMUN HOSSAIN <49454465+mamunsust12@users.noreply.github.com>
2024-12-02 12:54:02 +0600

    Add files via upload

---
+++ 10 - Files/Bubble_Sort/Dynamic_Bubble_sort_with_file_0.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    FILE * fp_in, * fp_out;
+    int *arr,i,j,n,t;
+
+    fp_in = fopen("input.txt", "r");
+    fp_out = fopen("output.txt", "w");
+
+    printf("How many  value:\n");
+        fscanf(fp_in,"%d",&n);
+        printf("%d\n",n);
+
+        arr=(int*)malloc(n*sizeof(int));
+        //arr=(int*)calloc(n,sizeof(int));
+
+        printf("Given   values:\n");
+        for(i=0; i<n; i++){
+            fscanf(fp_in,"%d",&arr[i]);
+            printf("%d ",arr[i]);
+        }
+
+    for(i=0; i<n; i++){
+        for(j=0; j<n-i-1; j++)
+            if(arr[j]>arr[j+1])
+            {
+                t=arr[j];
+                arr[j]=arr[j+1];
+                arr[j+1]=t;
+            }
+    }
+    printf("\nSorted  value:\n");
+    for(i=0; i<n; i++){
+        printf("%d ",arr[i]);
+        fprintf(fp_out,"%d ",arr[i]);
+    }
+
+    free(arr);
+    fclose(fp_in);
+    fclose(fp_out);
+    return 0;
+}
